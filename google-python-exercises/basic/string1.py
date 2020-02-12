3a46b63ff6ba3e4a2c6d6854631466a81155f186
Alpha <60446418+Valenciahub@users.noreply.github.com>
2020-02-12 12:29:51 -0800

    Update string1.py

--- google-python-exercises/basic/string1.py
+++ google-python-exercises/basic/string1.py
@@ -85,7 +85,7 @@ def main():
     test(donuts(10), 'Number of donuts: many')
     test(donuts(99), 'Number of donuts: many')
 
-    print
+    print()
     print('both_ends')
     test(both_ends('spring'), 'spng')
     test(both_ends('Hello'), 'Helo')
@@ -93,14 +93,14 @@ def main():
     test(both_ends('xyz'), 'xyyz')
 
   
-    print
+    print()
     print('fix_start')
     test(fix_start('babble'), 'ba**le')
     test(fix_start('aardvark'), 'a*rdv*rk')
     test(fix_start('google'), 'goo*le')
     test(fix_start('donut'), 'donut')
 
-    print
+    print()
     print('mix_up')
     test(mix_up('mix', 'pod'), 'pox mid')
     test(mix_up('dog', 'dinner'), 'dig donner')
